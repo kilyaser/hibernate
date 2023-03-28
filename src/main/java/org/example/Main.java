0e7003efeffc6603b0e324ecefc87c711260ea79
arcadag <piknik0711@yandex.ru>
2023-03-28 23:58:02 +0300

    homework lesson 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import org.example.lesson_5.entity.Student;
+import org.example.lesson_5.repository.StudentDAO;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StudentDAO studentDAO = new StudentDAO();
+
+        for (int i = 1; i < 1000; i++) {
+            Student student = new Student("Student" + i, "A");
+            studentDAO.save(student);
+        }
+        List<Student> students = studentDAO.findAll();
+        System.out.println(students.size());
+        studentDAO.deleteById(100L);
+        System.out.println(students.size());
+
+        Student studentDB = studentDAO.findById(10L);
+        System.out.println(studentDB);
+        studentDB.setName("Uniq");
+        studentDB.setMark("F");
+        System.out.println(studentDB);
+        studentDAO.deleteById(10L);
+
+
+
     }
 }
---
+++ src/main/java/org/example/lesson_5/entity/Student.java
@@ -0,0 +1,71 @@
+package org.example.lesson_5.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "mark")
+    private String mark;
+
+    public Student() {
+    }
+
+    public Student(String name, String mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(mark, student.mark);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, mark);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", mark='" + mark + '\'' +
+                '}';
+    }
+}
